e306eceac538191f0cbeb7778dd33916efac49bb
Raul <106830985+crewskt@users.noreply.github.com>
2023-11-24 17:23:13 -0300

    1

--- pop/js/editar.js
+++ pop/js/editar.js
@@ -1,12 +1,12 @@
       bioEp.init({
             html: '<div id="bio_ep_content">' +
-            '<img src="pop-up/imagens-pop/seta.png" width="54" alt="Participe" style="margin-right: 130px;" />' +
+            '<img src="pop/imagens-pop/seta.png" width="54" alt="Participe" style="margin-right: 130px;" />' +
             '<span></span>' +
             '<span></span>' +
             '<span></span>' +
-            '<a href="https://www.instagram.com/p/B2X-1Z_gGZA/?utm_source=ig_web_copy_link" target="_blank" class="bio_btn">Participar do Sorteio &nbsp; <i class="fa fa-instagram"></i></a>' +
+            '<a href="https://export-download.canva.com/3wZ7A/DAF1HE3wZ7A/4/0/0001-2318638149425022388.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJHKNGJLC2J7OGJ6Q%2F20231124%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231124T172808Z&X-Amz-Expires=16616&X-Amz-Signature=945eec90b95ac519b6c6ff7e488961fc999a67ff014525da8cbd069c18f0cff7&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27Card%25C3%25A1pio%2520de%2520Natal%2520Delicado%2520Story.png&response-expires=Fri%2C%2024%20Nov%202023%2022%3A05%3A04%20GMT" target="_blank" class="bio_btn">Participar do Sorteio &nbsp; <i class="fa fa-instagram"></i></a>' +
             '</div>',
-            css: '#bio_ep {width: 400px; height: 400px; color: #333; background-color: #fafafa; text-align: center;background-image: url(pop-up/imagens-pop/sorteio04.jpg);background-size: cover;}' +
+            css: '#bio_ep {width: 400px; height: 400px; color: #333; background-color: #fafafa; text-align: center;background-image: url(pop/imagens-pop/cardapio.png);background-size: cover;}' +
             '#bio_ep_content {padding: 255px 0 0 0; font-family: inherit;}' +
             '#bio_ep_content span:nth-child(2) {display: block; color: #f21b1b; font-size: 32px; font-weight: 600;}' +
             '#bio_ep_content span:nth-child(3) {display: block; font-size: 16px;}' +
